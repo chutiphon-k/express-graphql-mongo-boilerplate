01a3692830a704208997e75d34d42a7b74fb3bef
Chutiphon Kongsompot <chutiphon.k@storylog.co>
2017-07-30 20:10:57 +0700

    Remove eslint (#12)
    * Export eslintrc from index.js
    
    * Config eslint
    
    * Config babel
    
    * Config express server
    
    * Config ci
    
    * Remove node_modules
    
    * Add eslint-config-chutiphonk
    
    * Add eslint-config-chutiphonk
    
    * Add example graphql
    
    * Remove eslint-config-chutiphonk

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,46 @@
 import { Router } from 'express'
+import graphqlHTTP from 'express-graphql'
+import { formatError, createError } from 'apollo-errors'
+
+// import schema from 'schema'
+import { makeExecutableSchema } from 'graphql-tools'
+
+const typeDefs = `
+  type Query {
+	hello(name: String!): String
+  }
+
+  schema {
+	query: Query
+  }
+`
+const resolvers = {
+	Query: {
+		hello (obj, args) {
+			const { name } = args
+			if (name && name.toLowerCase() === 'james') {
+				const FooError = createError('Hello', {
+					message: 'Nooooo'
+				})
+				throw new FooError()
+			}
+			return `Hi ${name} !!!`
+		}
+	}
+}
+
+const schema = makeExecutableSchema({ typeDefs, resolvers })
 
 const router = Router()
 
 router.get('/', (req, res) => {
 	res.send('Hello')
 })
+
+router.use('/graphql', graphqlHTTP({
+	schema,
+	formatError,
+	graphiql: process.env.NODE_ENV !== 'production'
+}))
 
 export default router
